31e04057944a07772c5ac2515e7d3da337084285
PuhlUp <lucas8puhl@gmail.com>
2023-11-11 16:48:21 -0300

    First step on 03-task-manager project

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,22 @@
 console.log('Task Manager App')
+
+const express = require('express');
+const app =  express();
+const tasks = require('./routes/tasks')
+
+// midleware
+app.use(express.json())
+
+
+// routes 
+app.get('/hello', (req,res) => {
+    res.send('Task Manager App')
+}) 
+
+app.use('/api/v1/tasks', tasks)
+
+// app.get('/api/v1/tasks')  // get all the tasks
+
+const port = 3000;
+app.listen(port, console.log(`Server is listening on port ${port}... `))
+ 
---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,30 @@
+
+
+const getAllTasks = (req, res) => {
+    res.send('get all tasks')
+}
+
+const createTask = (req, res) => {
+    // res.send('create task')
+    res.json(req.body)
+}
+
+const getTask = (req, res) => {
+    res.send('get task')
+}
+
+const updateTask = (req, res) => {
+    res.send('update task')
+}
+
+const deleteTask = (req, res) => {
+    res.send('delete task')
+}
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+}
