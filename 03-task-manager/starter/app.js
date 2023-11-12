cdd89600620d11333cbe356302b964bb39401cf0
PuhlUp <lucas8puhl@gmail.com>
2023-11-12 11:13:06 -0300

    using .env

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -4,10 +4,9 @@ const express = require('express');
 const app =  express();
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
-const {
-    dbLogin, 
-    connectionsString,
-} = require('./db/dbKeys')
+
+require('dotenv').config()
+
 
 // midleware
 app.use(express.json())
@@ -28,11 +27,9 @@ const port = 3000;
 
 const start = async() => {
     try {
-        // warning: don't log the pass
-        // console.log(`URL db connecting: ${connectionsString} `);
-        await connectDB(connectionsString)
-        app.listen(port, console.log(`Server is listening on port ${port}... `))
-        console.log(`Connected to the DataBase Atlas Mongoose => ${dbLogin.dbName}`)
+        await connectDB( process.env.MONGO_URI )
+        app.listen( port, console.log(`Server is listening on port ${port}... `) )
+        console.log('Connected to the DataBase Atlas Mongoose')
     } catch (error) {
         console.log(error);
     }
--- 03-task-manager/starter/db/dbKeysEx.js
+++ 03-task-manager/starter/db/dbKeysEx.js
@@ -5,4 +5,9 @@ const dbLogin = {
     server: 'server'
 };
 
-module.exports = dbLogin;
+const connectionsString = `mongodb+srv://${dbLogin.user}:${dbLogin.key}@nodeexpressproject.x5jedqa.mongodb.net/${dbLogin.dbName}?retryWrites=true&w=majority`;
+
+module.exports = {
+    dbLogin, 
+    connectionsString,
+}
